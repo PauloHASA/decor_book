a151f9f6d32fdedc5387eca5816da3e3d5f04ee9
Paulo Hasa <paulohasa@gmail.com>
2024-01-28 23:26:17 -0300

    FEAT: building the front from the portfolio app

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,6 +2,11 @@
 from django.urls import path, include
 from . import views
 
+app_name = "portfolio"
+
 urlpatterns = [
-    path('', views.timeline_portfolio, name='timeline_portfolio.html'),
+    path('', views.timeline_portfolio, name='timeline_portfolio'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('portfolio/store_portfolio/', views.store_portfolio, name='store_portfolio'),
+    path('portfolio/product_page/', views.produt_page, name='product_page'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def timeline_portfolio(request):
     return render(request,'timeline_portfolio.html')
+
+def portfolio(request):
+    return render(request,'portfolio.html')
+
+def store_portfolio(request):
+    return render(request,'store-portfolio.html')
+
+def produt_page(request):
+    return render(request,'product-page.html')
