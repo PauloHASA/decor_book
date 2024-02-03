fab2887a5834d8b6d37210465e15e1765ff06146
PauloHasa <paulohasa@gmail.com>
2024-02-03 18:13:38 -0300

    FEAT: Building a front end pages

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,12 +1,19 @@
 
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 app_name = "portfolio"
 
 urlpatterns = [
-    path('', views.timeline_portfolio, name='timeline_portfolio'),
-    path('portfolio/', views.portfolio, name='portfolio'),
-    path('portfolio/store_portfolio/', views.store_portfolio, name='store_portfolio'),
-    path('portfolio/product_page/', views.produt_page, name='product_page'),
+    path("", views.timeline_portfolio, name="timeline_portfolio"),
+    path("portfolio/", views.portfolio, name="portfolio"),
+    path("my-projects/", views.my_projects, name="my_projects"),
+    path("store_portfolio/", views.store_portfolio, name="store_portfolio"),
+    path("project_page/", views.project_page, name="project_page"),
+    
+    path("home_page/", views.home_page, name="home_page"),
+    path("new_project_step1/", views.new_project_step1, name="new_project_step1"),
+    path("new_project_step2/", views.new_project_step2, name="new_project_step2"),
+    path("new_project_step3/", views.new_project_step3, name="new_project_step3"),
+    
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -5,11 +5,26 @@ from django.shortcuts import render
 def timeline_portfolio(request):
     return render(request,'timeline_portfolio.html')
 
+def my_projects(request):
+    return render(request,'my-projects.html')
+
 def portfolio(request):
     return render(request,'portfolio.html')
 
 def store_portfolio(request):
-    return render(request,'store-portfolio.html')
+    return render(request,'store_portfolio.html')
 
-def produt_page(request):
-    return render(request,'product-page.html')
+def project_page(request):
+    return render(request,'project-page.html')
+
+def home_page(request):
+    return render(request,'home-page.html')
+
+def new_project_step1(request): 
+    return render(request,'new-project-step1.html')
+
+def new_project_step2(request):
+    return render(request,'new-project-step2.html')
+
+def new_project_step3(request):
+    return render(request,'new-project-step3.html')
