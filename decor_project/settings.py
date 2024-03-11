74052dfdfb4c5132cc6f7fef629f782954847b60
Paulo Hasa <paulohasa@gmail.com>
2024-03-11 09:51:12 -0300

    FEAT: Alloewes hosts in settings.py

--- decor_project/settings.py
+++ decor_project/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = 'django-insecure-d1@i0iv_9m*!5qds=%hai5(flp%em_xbwo3ltz@aoeowx#p0e6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.decorbook.com.br',
+                 'www.decorbook.com.br:8000',
+                 'decorbook.com.br:8000',
+                 'decorbook.com.br'
+                 ]
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
