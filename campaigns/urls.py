b4aca186b7977a8297639da335eff58e6f172f70
PauloHasa <paulohasa@gmail.com>
2024-06-04 22:57:55 -0300

    FEATURE: Email automations cod

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -7,6 +7,6 @@ app_name = "campaigns"
 urlpatterns = [
     path('send_email/', views.send_test_email, name='send_email'),
     path('email_build/', views.email_build, name='email_build'),
-    path('main/', views.main, name='main'),
+    path('email_test/', views.email_test, name='email_test'),
 ]
 
--- campaigns/views.py
+++ campaigns/views.py
@@ -4,44 +4,47 @@ from .email_utils import send_custom_email
 
 import pandas as pd
 from campaigns.models import Dataframe
+import uuid
 
 def send_test_email(request):
-    subject = 'Test de envio de email'
-    message = 'Este e um email de teste'
-    recipient_list = ['paulohasa@gmail.com']
+    dataframe = Dataframe.objects.filter(request_id = 1)
     
-    send_custom_email(subject,
-                      message,
-                      recipient_list
-                      )
+    total_emails = len(dataframe)
+    email_sent = 0
+    email_faled = 0
+    
+    for df in dataframe:
+        unique_id = uuid.uuid4()  
+        subject = f"Seu Assunto Aqui - ID: {unique_id}"
+        message = f"Seu conteúdo de mensagem aqui - ID: {unique_id}"
+        subject = 'Test de envio de email'
+        message = 'Este e um email de teste'
+        recipient_list = [df.email]
+        
+        try:
+            send_custom_email(subject,
+                            message,
+                            recipient_list,
+                            )
+            print(f"E-mail enviado para {df.email} - OK")
+            email_sent += 1
+        except Exception as e:
+            print(f"Erro ao enviar e-mail para {df.email}: {e}")
+            email_faled += 1
+            
+        print(f"------------------------------------------------")
+        print(f"Total de e-mails processados: {total_emails}")
+        print(f"E-mails enviados com sucesso: {email_sent}")
+        print(f"E-mails que falharam: {email_faled}")
+        print(f"------------------------------------------------")
+
     
     return HttpResponse("E-mail enviado com sucesso!")
 
-
+        
 def email_build(request):
     
     return render (request, "email_build.html")
 
-
-def main(request):
-    file_path = r'media\email_list_test.csv'
-    df = pd.read_csv(file_path)
-
-    for index, row in df.iterrows():
-        dataframe_instance = Dataframe()
-        
-        dataframe_instance.request_id = 1
-        dataframe_instance.admin_id = row['ADMIN_ID']
-        dataframe_instance.name = row['PRFSNL_ORG_RGSTRD_NM']
-        dataframe_instance.addr_pstl_cd = row['ADDR_PSTL_CD']
-        dataframe_instance.latitude = row['LATITUDE']
-        dataframe_instance.longitude = row['LONGITUDE']
-        dataframe_instance.coord_geoc = row['GEOC']
-        dataframe_instance.email = row['EMAIL']
-
-        dataframe_instance.save()
-        
-    if __name__ == "__main__":
-        main()
-        
-    return HttpResponse("Deu certo !")
+def email_test(request):
+    return render( request, 'test.html')
