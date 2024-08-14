dd964e6779b327cd71373b2fe7ef7c27087eeaba
Paulo Hasa <paulohasa@gmail.com>
2024-08-14 13:07:55 -0300

    FEAT: Deploy config

--- decor_project/settings.py
+++ decor_project/settings.py
@@ -35,13 +35,15 @@ SECRET_KEY = 'django-insecure-d1@i0iv_9m*!5qds=%hai5(flp%em_xbwo3ltz@aoeowx#p0e6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-# ALLOWED_HOSTS = ['www.decorbook.com.br',
-#                  'www.decorbook.com.br:8000',
-#                  'decorbook.com.br:8000',
-#                  'decorbook.com.br'
-#                  ]
+ALLOWED_HOSTS = ['www.decorbook.com.br',
+                 'www.decorbook.com.br:8000',
+                 'decorbook.com.br:8000',
+                 'decorbook.com.br'
+                 ]
 
-ALLOWED_HOSTS = []
+
+# ALLOWED_HOSTS = []
+
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
